4820b35a456b13a1fa5189352f0eaa7b306088ac
bao longhui <547525253@qq.com>
2022-04-16 18:18:21 +0800

    chore(_app.tsx,globals.css): 引入全局样式

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import 'antd/dist/antd.css'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
