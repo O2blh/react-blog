1196dd04aeef9cb5a11e0356b0c366ef2980cc80
bao longhui <547525253@qq.com>
2022-04-18 12:09:30 +0800

    feat(pages): 首页静态页面部分实现

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-function HomePage() {
-  return <div className='sds'>Welcome to Next.js!</div>
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import styles from '@/styles/index.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.center}>
+      <Head>
+        <title>伊之助</title>
+        <meta name='description' content='伊之助的博客' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <div className={styles.box}>
+        <div className={styles.title}>伊之助的博客</div>
+        <div className={styles.desc}>千山鸟飞绝, 万径人踪灭。</div>
+      </div>
+      <div className={styles.body} />
+    </div>
+  )
 }
 
-export default HomePage
+export default Home
