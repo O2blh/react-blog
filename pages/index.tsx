b2401d05d24ecc70236c21adac8152448e26a8f5
bao longhui <547525253@qq.com>
2022-04-19 16:00:54 +0800

    feat(pages): 首页引入ArticleCard组件和WelcomeCard组件

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '@/styles/index.module.css'
+import ArticleCard from '@/components/Home/ArticleCard'
+import WelcomeCard from '@/components/Home/WelcomeCard'
 
 const Home: NextPage = () => {
   return (
@@ -11,10 +13,17 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className={styles.box}>
-        <div className={styles.title}>伊之助的博客</div>
+        <div className={styles.name}>伊之助的博客</div>
         <div className={styles.desc}>千山鸟飞绝, 万径人踪灭。</div>
       </div>
-      <div className={styles.body} />
+      <div className={styles.body}>
+        <section className={styles.section}>
+          <ArticleCard />
+        </section>
+        <aside className={styles.aside}>
+          <WelcomeCard />
+        </aside>
+      </div>
     </div>
   )
 }
