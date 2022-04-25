d13b107bf668299160c17aa3fd17399213fdc5e4
bao longhui <547525253@qq.com>
2022-04-25 16:42:06 +0800

    feat(components): 一些样式调整

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import styles from './style.module.css'
 
 const Footer = () => {
-  const tags = ['React', 'React Router', 'Redux', 'Webpack', 'Antd', 'ahooks', 'CloudBase']
+  const tags = ['React', 'NextJs', 'TypeScript', 'Antd', 'ahooks', 'CloudBase']
 
   return (
     <div className={styles.footer}>
-      <div className=''>
+      <div className={styles.footerTitle}>
         个人博客系统<a href='https://gitee.com/bao-longhui/react-blog.git'>「源代码」</a>
       </div>
-      <div className=''>
+      <div>
         <a href='https://beian.miit.gov.cn/'>浙ICP备2022010990号-1</a>
       </div>
-      <div className=''>
+      <div>
         {tags.map((tag) => {
           return (
             <span key={tag} className={styles.tag}>
