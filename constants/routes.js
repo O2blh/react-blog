1195e43c9c37052c65db018d32b7a11e70afe7ea
bao longhui <547525253@qq.com>
2022-04-25 16:21:39 +0800

    feat(constants): 添加一个常量文件存放路由信息

---
+++ constants/routes.js
@@ -0,0 +1,26 @@
+const ROOT = '/'
+const HOME = '/home'
+const ARTICLES = '/articles'
+const GALLERY = '/gallery'
+const SAY = '/say'
+const MSG = '/msg'
+const FRIEND_LINK = '/friendLink'
+const WORKS = '/works'
+const ABOUT = '/about'
+const WEBSITE_LOGS = '/websiteLogs'
+const DEFAULT = ROOT
+
+const ROUTES = {
+  ROOT,
+  HOME,
+  ARTICLES,
+  GALLERY,
+  SAY,
+  MSG,
+  FRIEND_LINK,
+  WORKS,
+  ABOUT,
+  WEBSITE_LOGS,
+  DEFAULT,
+}
+export default ROUTES
