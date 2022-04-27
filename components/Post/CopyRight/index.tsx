a412e384bebe3254759c4b7935deb55ae54da242
bao longhui <547525253@qq.com>
2022-04-27 16:23:00 +0800

    feat(components): 文章详情-copyRight组件

---
+++ components/Post/CopyRight/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { SITE_NAME } from '@/constants/siteInfo'
+import copy from 'copy-to-clipboard'
+import { message } from 'antd'
+import CopyIcon from './CopyIcon'
+import CopyrightIcon from './CopyrightIcon'
+import styles from './style.module.css'
+
+const CopyRight = ({ title, link }) => {
+  const copyLink = () => {
+    if (copy(link)) {
+      message.success('复制成功')
+    }
+  }
+
+  return (
+    <div className={styles.copyRight}>
+      <div className={styles.articleTitle}>{title}</div>
+      <div className={styles.articleLink}>
+        {link}
+        <div className={styles.copyBtn} onClick={copyLink}>
+          <CopyIcon />
+        </div>
+      </div>
+      <div className={styles.statement}>
+        本站所有文章除特别声明外,均采用
+        <a
+          className={styles.agreement}
+          href='https://creativecommons.org/licenses/by-nc-sa/4.0/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          CC BY-NC-SA 4.0
+        </a>
+        许可协议，转载请注明来自<span className={styles.sitename}>{SITE_NAME}</span>。
+      </div>
+      <CopyrightIcon className={styles.copyrightIcon} />
+    </div>
+  )
+}
+
+export default CopyRight
