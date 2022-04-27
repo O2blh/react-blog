caf340d614443d074e9423ef68f0aecfb2c14f56
bao longhui <547525253@qq.com>
2022-04-27 16:13:55 +0800

    feat(): 首页文章卡片点击跳转详情页

--- components/Home/ArticleCard/index.tsx
+++ components/Home/ArticleCard/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import dayjs from 'dayjs'
+import Link from 'next/link'
 import cardStyles from '@/styles/card.module.css'
 import styles from './style.module.css'
 
@@ -9,22 +10,24 @@ const ArticleCard = ({ articles }) => {
     <>
       {articles.map((article) => {
         return (
-          <div key={article._id} className={classNames(cardStyles.card, styles.article)}>
-            <div className={styles.title}>{article.articleTitle}</div>
-            <p className={styles.content}>
-              {article.articleContent.replace(/<a(.*?)>(.*?)<\/a>/g, '$2').replace(/[# |**|`|>]/g, '')}
-            </p>
-            <div className={styles.info}>
-              <span className={classNames(styles.tag, styles.date)}>
-                {dayjs(article.modifyDate).format('YYYY-MM-DD')}
-              </span>
-              {article.tags.map((tag) => (
-                <span key={tag} className={styles.tag}>
-                  {tag}
+          <Link key={article._id} href={`/posts/${article._id}`}>
+            <div className={classNames(cardStyles.card, styles.article)}>
+              <div className={styles.title}>{article.articleTitle}</div>
+              <p className={styles.content}>
+                {article.articleContent.replace(/<a(.*?)>(.*?)<\/a>/g, '$2').replace(/[# |**|`|>]/g, '')}
+              </p>
+              <div className={styles.info}>
+                <span className={classNames(styles.tag, styles.date)}>
+                  {dayjs(article.modifyDate).format('YYYY-MM-DD')}
                 </span>
-              ))}
+                {article.tags.map((tag) => (
+                  <span key={tag} className={styles.tag}>
+                    {tag}
+                  </span>
+                ))}
+              </div>
             </div>
-          </div>
+          </Link>
         )
       })}
     </>
