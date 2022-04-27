e35d9516a80a7d4ea37da74005546c5318b4e483
bao longhui <547525253@qq.com>
2022-04-27 16:11:59 +0800

    feat(api): api新增
    增加api getAllArticle,getAllArticleId
    getArticles 重命名为 getArticlesPagination

--- request/api.js
+++ request/api.js
@@ -1,14 +1,31 @@
 import { service } from './index'
 
-const GET_ARTICLES = '/getArticles'
+const GET_ARTICLES_PAGINATION = '/getArticlesPagination'
 const GET_ARTICLE_BY_ID = '/getArticleById'
+const GET_ALL_ARTICLE = '/getAllArticle'
 const GET_CLASSIFIES = '/getClassifies'
 const GET_TAGS = '/getTags'
 const GET_NOTICE = '/getNotice'
 
-export const getArticles = async ({ page = 1, limit = 10 }) => {
-  const res = await service.get(`${GET_ARTICLES}?page=${page}&limit=${limit}`)
+export const getArticlesPagination = async ({ page = 1, limit = 10 }) => {
+  const res = await service.get(`${GET_ARTICLES_PAGINATION}?page=${page}&limit=${limit}`)
   return res.data
+}
+
+export const getAllArticle = async () => {
+  const res = await service.get(GET_ALL_ARTICLE)
+  return res.data
+}
+
+export const getAllArticleId = async () => {
+  const res = await service.get(GET_ALL_ARTICLE)
+  return res.data.map((item) => {
+    return {
+      params: {
+        id: item._id,
+      },
+    }
+  })
 }
 
 export const getArticleById = async (id) => {
